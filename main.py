c1020a92c69a70252da1365b6fe2dd3a0818dd2e
Adeline-Liu <yifei05@outlook.com>
2023-05-31 00:07:28 +0800

    Drew windows

--- main.py
+++ main.py
@@ -19,7 +19,8 @@ block_size = 30
 top_left_x = (s_width - play_width) // 2
 top_left_y = s_height - play_height
 
-# SHAPE FORMATS
+# SHAPE FORMAT
+# 0 represents where the block actually exists
 
 S = [['.....',
       '.....',
@@ -124,7 +125,59 @@ T = [['.....',
       '.....']]
 
 shapes = [S, Z, I, O, J, L, T]
-shape_colors = [(0, 255, 0), (255, 0, 0), (0, 255, 255), (255, 255, 0), (255, 165, 0), (0, 0, 255), (128, 0, 128)]
+# corresponding colors to shapes
+shape_colors = [(0, 255, 0),
+                (255, 0, 0),
+                (0, 255, 255),
+                (255, 255, 0),
+                (255, 165, 0),
+                (0, 0, 255),
+                (128, 0, 128)]
 # index 0 - 6 represent shape
+
+class Piece(object):
+    def __init__(self, x, y, shape):
+        self.x = x
+        self.y = y
+        self.shape = shape
+        self.color = shape_colors[shapes.index(shape)]
+        self.rotation = 0
+
+def create_grid(locked_pos = {}):
+    grid = [[(0, 0, 0) for x in range(10)] for x in range(20)]
+
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if (j, i) in locked_pos:
+                c = locked_pos[(j, i)]
+                grid[i][j] = c
+    return grid
+
+# return a random shape in the shape list
+def get_shape():
+    return random.choice(shapes)
+
+def draw_grid(surface, grid):
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            pygame.draw.rect(surface, grid[i][j],
+                             (top_left_x + j * block_size,
+                              top_left_y + i * block_size,
+                              block_size, block_size), 0)
+
+    pygame.draw.rect(surface, (255, 0, 0), (top_left_x, top_left_y, play_width, play_height), 4)
+
+def draw_window(surface, grid):
+    # black surface
+    surface.fill((0, 0, 0))
+
+    font = pygame.font.SysFont('Arial', 60)
+    label = font.render('TETRIS', 1, (255, 255, 255))
+
+    surface.blit(label, (top_left_x + play_width / 2 - (label.get_width() / 2), 30))
+
+    draw_grid(surface, grid)
+
+    pygame.display.update()
 
 
